5ee5df9fcfe009b12a804b185f45032eca10776c
TheRonTron <raflores412@gmail.com>
2024-11-09 15:40:25 -0600

    Initial commit

---
+++ ABadBaguette.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using Jotunn.Configs;
+using Jotunn.Entities;
+using Jotunn.Managers;
+using Jotunn.Utils;
+using UnityEngine;
+
+namespace ABadBaguette
+{
+    [BepInPlugin("TheRonTron.BadBaguette", "ABadBaguette", "4.2.0")]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    internal class ABadBaguette : BaseUnityPlugin
+    {
+        private AssetBundle BadBaguette;
+        public void Awake()
+        {
+            Jotunn.Logger.ShowDate = true;
+            Game.isModded = true;
+
+            Jotunn.Logger.LogInfo($"Embedded resources: {string.Join(",", typeof(ABadBaguette).Assembly.GetManifestResourceNames())}");
+            BadBaguette = AssetUtils.LoadAssetBundleFromResources("badbaguette", typeof(ABadBaguette).Assembly);
+
+            var ABadBaguette = BadBaguette.LoadAsset<GameObject>("BadBaguette");
+            ItemConfig BreadConfig = new ItemConfig();
+
+            var incineratorConfig = new IncineratorConversionConfig();
+            incineratorConfig.Requirements.Add(new IncineratorRequirementConfig("BreadDough", 1));
+            incineratorConfig.Requirements.Add(new IncineratorRequirementConfig("SwordBronze", 1));
+            incineratorConfig.ToItem = "BadBaguette";
+            incineratorConfig.Station = Incinerators.Incinerator;
+            incineratorConfig.ProducedItems = 1;
+            incineratorConfig.RequireOnlyOneIngredient = false;
+            incineratorConfig.Priority = 5;
+            ItemManager.Instance.AddItemConversion(new CustomItemConversion(incineratorConfig));
+
+            ItemManager.Instance.AddItem(new CustomItem(ABadBaguette, fixReference: true, BreadConfig));
+
+            BadBaguette.Unload(false);
+        }
+    }
+}
